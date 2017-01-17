5bbe214018fe5bc8ea7895acd0180a0eab6e14ca
Sérgio Gomes <sgomes@google.com>
2017-01-17 16:38:34 +0000

    Update 2017-01-17T16:38:20.727Z

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const shell = require('gulp-shell');
+const csso = require('gulp-csso');
+const ghPages = require('gulp-gh-pages');
+
+const logo = 'assets/images/logo.svg';
+const touchDir = '_site/assets/images/touch';
+
+gulp.task('clean', shell.task(['rm -rf _site']));
+
+gulp.task('create-touch-dir', shell.task(['mkdir -p _site/assets/images/touch']));
+
+gulp.task('generate-touch', ['create-touch-dir'],
+  shell.task([16, 32, 144, 152, 192].map(size =>
+    `convert -background none -density 512 -resize ${size}x ${logo} ${touchDir}/${size}x${size}.png`
+  ))
+);
+
+gulp.task('sass', ['generate-touch'], () => gulp.src('./_sass/main.scss')
+  .pipe(sass({includePaths: 'node_modules'}).on('error', sass.logError))
+  .pipe(csso())
+  .pipe(gulp.dest('./_includes'))
+);
+
+gulp.task('build', ['sass']);
+
+gulp.task('serve', ['build'], shell.task(['bundle exec jekyll serve']));
+
+gulp.task('deploy', ['build'], () => {
+  return gulp.src('./**/*')
+    .pipe(ghPages());
+});
