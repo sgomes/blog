06772dcbaacac533ef105b34a2e9e580cc645557
Sérgio Gomes <sgomes@google.com>
2017-01-18 18:44:20 +0000

    Major service worker revamp

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'sgomes-v5';
+const CACHE_NAME = 'sgomes-v6';
+const TIMEOUT = 4;
 
 // Cached files
-const siteAssets = [
+const SITE_ASSETS = [
   '/favicon.ico',
   '/assets/images/avatar.jpg',
   '/assets/images/email.svg',
@@ -19,13 +20,14 @@ const siteAssets = [
   '/assets/fonts/d-6IYplOFocCacKzxwXSOJBw1xU1rKptJj_0jans920.woff2',
   '/assets/fonts/vPcynSL0qHq_6dX7lKVByXYhjbSpvc47ee6xR_80Hnw.woff2',
   '/assets/fonts/OLffGBTaF0XFOW1gnuHF0Ygp9Q8gbYrhqGlRav_IXfk.woff2',
-  '/assets/fonts/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2'
+  '/assets/fonts/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2',
+  '/manifest.json'
 ];
 
 // Install essential URLs.
 self.addEventListener('install', event => {
   event.waitUntil(
-      caches.open(CACHE_NAME).then(cache => cache.addAll(siteAssets)));
+      caches.open(CACHE_NAME).then(cache => cache.addAll(SITE_ASSETS)));
 });
 
 // Delete old caches.
@@ -42,19 +44,69 @@ self.addEventListener('activate', event => {
 
 // Fetch data from cache.
 self.addEventListener('fetch', event => {
-  // Respond immediately from cache.
-  event.respondWith(fromCache(event.request)
-    .catch(() => fetch(event.request)));
-
-  // Update the cache resource?
-  let match = siteAssets.find(url => event.request.url === location.origin + url);
-  if (!match) {
-    // Not a site asset. Load from network and store in cache.
-    event.waitUntil(update(event.request));
-  } else {
-    // Site asset, don't update.
+  const requestUrl = new URL(event.request.url);
+  // Routing for local URLs
+  if (requestUrl.origin == location.origin) {
+    if (SITE_ASSETS.find(url => requestUrl.pathname === url)) {
+      // Installed asset. Cache only, with network fallback.
+      cacheWithNetworkFallback(event);
+    } else if (requestUrl.pathname === '/' || requestUrl.pathname === '/index.html') {
+      // Home. Try network first, with timeout. If it times out, go to cache.
+      networkWithTimeoutAndUpdate(event);
+    } else if (/\.html$/.test(requestUrl.pathname)) {
+      // Article. Try network first, with timeout. If it times out, go to cache.
+      networkWithTimeoutAndUpdate(event, true);
+    } else {
+      // Cache, then update.
+      cacheThenUpdate(event);
+    }
   }
 });
+
+
+// Auxiliary methods.
+
+// Use first promise to resolve, reject if they all do.
+function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    // make sure promises are all promises
+    promises = promises.map(p => Promise.resolve(p));
+    // resolve this promise as soon as one resolves
+    promises.forEach(p => p.then(resolve));
+    // reject if all promises reject
+    promises.reduce((a, b) => a.catch(() => b))
+      .catch(() => reject(Error("All failed")));
+  });
+};
+
+// Network with timeout loading strategy, and update logic.
+// Attempts to load from the network, with a timeout.
+// If the timeout happens, it loads from cache immediately, or waits for
+// network if the resource isn't in the cache.
+// In parallel, it caches the new network response (if successful).
+function networkWithTimeoutAndUpdate(event) {
+  const cached = fromCache(event.request);
+  const network = fetch(event.request);
+  const networkClone = network.then(response => response.clone());
+
+  event.respondWith(promiseAny([
+    networkClone.catch(() => cached),
+    wait(TIMEOUT).then(() => cached)
+  ]));
+  event.waitUntil(network.then(resp => update(event.request, resp)));
+}
+
+function cacheWithNetworkFallback(event) {
+  event.respondWith(fromCache(event.request).catch(() => fetch(event.request)));
+}
+
+function cacheThenUpdate(event) {
+  const network = fetch(event.request);
+  const networkClone = network.then(response => response.clone());
+
+  event.respondWith(fromCache(event.request).catch(() => networkClone));
+  event.waitUntil(network.then(resp => update(event.request, resp)));
+}
 
 function fromCache(request) {
   return caches.open(CACHE_NAME).then(cache => {
@@ -64,10 +116,10 @@ function fromCache(request) {
   });
 }
 
-function update(request) {
-  return caches.open(CACHE_NAME).then(cache => {
-    return fetch(request).then(response => {
-      return cache.put(request, response);
-    });
-  });
+function wait(timeout) {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
+function update(request, response) {
+  return caches.open(CACHE_NAME).then(cache => cache.put(request, response));
 }
